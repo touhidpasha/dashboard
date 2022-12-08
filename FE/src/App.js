969fc9de1bcba15860f7029ac35665af47491b26
touhidpasha <touhidpasha552@gmail.com>
2022-12-08 15:46:34 +0530

    updated front-end code

---
+++ FE/src/App.js
@@ -0,0 +1,14 @@
+
+import logo from './logo.svg';
+import './App.css';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+     <Dashboard ></Dashboard>
+    </div>
+  );
+}
+
+export default App;
